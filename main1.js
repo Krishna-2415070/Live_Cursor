80c7f808458793628ada3c3ae5390947ad8b33b0
Krishna <krishnamahato9712@gmail.com>
2026-06-01 23:15:38 +0530

    INITIAL COMMIT

---
+++ main1.js
@@ -0,0 +1,6 @@
+var window = document.querySelector("#main");
+var crsr = document.querySelector(".cursor");
+window.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+});
